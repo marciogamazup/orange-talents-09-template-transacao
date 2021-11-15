05264981465e7b67e00371f660ef9ba3ce8bab63
marciogamazup <marcio.gama@zup.com.br>
2021-11-15 14:36:52 -0300

    Autenticação e Autorização

---
+++ src/main/java/br/com/zupacademy/marcio/transacao/transacao/commons/security/ResourceServerConfig.java
@@ -0,0 +1,22 @@
+package br.com.zupacademy.marcio.transacao.transacao.commons.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequests ->
+                authorizeRequests
+                        .antMatchers(HttpMethod.GET,"/transacoes/**").hasAuthority("SCOPE_escopo-transacao")
+                        .anyRequest().authenticated()
+        ).oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+}
